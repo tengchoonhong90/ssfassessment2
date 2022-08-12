0d6282b4e3d5d0c1ed4c99f7eb71fbcaff6f0bd9
Teng Choon Hong <tengchoonhong@Tengs-MacBook-Pro.local>
2022-08-12 09:07:51 +0800

    first commit

---
+++ src/main/java/com/ssfassessment2/cryptonews/CryptonewsApplication.java
@@ -0,0 +1,13 @@
+package com.ssfassessment2.cryptonews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptonewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptonewsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ssfassessment2/cryptonews/CryptonewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssfassessment2.cryptonews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptonewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
